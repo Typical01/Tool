1005260dd2fe6a26757e13819345d6170ce7cbef
TypicalOne <typicalnum@gmail.com>
2024-12-08 12:32:07 +0800

    修复: class StringManage 文件定义问题

--- src/StringManage.cpp
+++ src/StringManage.cpp
@@ -1,30 +1,109 @@
-#pragma once
+#include "StringManage.h"
 
-//���ù���
-#ifndef _STRINGMANAGE_H
-#define _STRINGMANAGE_H
-
-
-#include "pch.h"
-#include "Log.h"
-
-
-//�ַ�����---------------------------------------------------------------------------------------------------------------
-namespace StringManage
+std::wstring Typical_Tool::StringManage::StringToWstring(const std::string& str)
 {
+	std::wstring wContext;
 
-	//�ַ�ת��-------------------------------------------------------------------------------------------------------
-	std::wstring StringToWstring(const std::string& str);
-	std::wstring StringToWstring(std::string&& str);
-#define stow StringToWstring
-	std::string WstringToString(const std::wstring& wstr);
-	std::string WstringToString(std::wstring&& wstr);
-#define wtos WstringToString
+	if (str.empty()) {
+		return wContext;
+	}
 
-	//����ת��---------------------------------------------------------------------------------------------------------
+#ifdef _WINDOWS
+	// Windows 版本
+	int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.size(), nullptr, 0);
+	if (len <= 0) {
+		throw std::runtime_error("Failed to convert string to wide string.");
+	}
+	std::unique_ptr<wchar_t[]> buffer(new wchar_t[len + 1]);
+	if (MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.size(), buffer.get(), len) <= 0) {
+		throw std::runtime_error("Failed to convert string to wide string.");
+	}
+	buffer[len] = _T('\0');
+	wContext.assign(buffer.get());
+#else
+	lgcr("stow: 转换失败, 没有声明对应平台(_WINDOWS/_UNIX)", lm::er);
+#endif
+
+	return wContext;
 }
 
+std::wstring Typical_Tool::StringManage::StringToWstring(std::string&& str)
+{
+	std::wstring wContext;
 
+	if (str.empty()) {
+		return wContext;
+	}
 
-
+#ifdef _WINDOWS
+	// Windows 版本
+	int len = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.size(), nullptr, 0);
+	if (len <= 0) {
+		throw std::runtime_error("Failed to convert string to wide string.");
+	}
+	std::unique_ptr<wchar_t[]> buffer(new wchar_t[len + 1]);
+	if (MultiByteToWideChar(CP_UTF8, 0, str.c_str(), str.size(), buffer.get(), len) <= 0) {
+		throw std::runtime_error("Failed to convert string to wide string.");
+	}
+	buffer[len] = _T('\0');
+	wContext.assign(buffer.get());
+#else
+	lgcr("stow: 转换失败, 没有声明对应平台(_WINDOWS/_UNIX)", lm::er);
 #endif
+
+	return wContext;
+}
+
+std::string Typical_Tool::StringManage::WstringToString(const std::wstring& wStr)
+{
+	std::string context;
+
+	if (wStr.empty()) {
+		return context;
+	}
+
+#ifdef _WINDOWS
+	// Windows 版本
+	int len = WideCharToMultiByte(CP_UTF8, 0, wStr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	if (len <= 0) {
+		throw std::runtime_error("Failed to convert wide string to string.");
+	}
+	std::unique_ptr<char[]> buffer(new char[len + 1]);
+	if (WideCharToMultiByte(CP_UTF8, 0, wStr.c_str(), -1, buffer.get(), len, nullptr, nullptr) <= 0) {
+		throw std::runtime_error("Failed to convert wide string to string.");
+	}
+	buffer[len] = '\0';
+	context.assign(buffer.get());
+#else
+	lgcr("stow: 转换失败, 没有声明对应平台(_WINDOWS/_UNIX)", lm::er);
+#endif
+
+	return context;
+}
+
+std::string Typical_Tool::StringManage::WstringToString(std::wstring&& wStr)
+{
+	std::string context;
+
+	if (wStr.empty()) {
+		return context;
+	}
+
+#ifdef _WINDOWS
+	// Windows 版本
+	int len = WideCharToMultiByte(CP_UTF8, 0, wStr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+	if (len <= 0) {
+		throw std::runtime_error("Failed to convert wide string to string.");
+	}
+	std::unique_ptr<char[]> buffer(new char[len + 1]);
+	if (WideCharToMultiByte(CP_UTF8, 0, wStr.c_str(), -1, buffer.get(), len, nullptr, nullptr) <= 0) {
+		throw std::runtime_error("Failed to convert wide string to string.");
+	}
+	buffer[len] = '\0';
+	context.assign(buffer.get());
+#else
+	lgcr("stow: 转换失败, 没有声明对应平台(_WINDOWS/_UNIX)", lm::er);
+#endif
+
+	return context;
+}
